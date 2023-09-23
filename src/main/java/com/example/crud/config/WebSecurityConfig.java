fe6877691c04a324e4be92394f6fbbeb3d85545f
unknown <lukakavtaradze2001@gmail.com>
2023-09-24 03:41:46 +0400

    add role

--- src/main/java/com/example/crud/config/WebSecurityConfig.java
+++ src/main/java/com/example/crud/config/WebSecurityConfig.java
@@ -1,49 +1,88 @@
 package com.example.crud.config;
 
+import com.example.crud.repositories.IUsersRepository;
+import com.example.crud.services.impl.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.swing.plaf.PanelUI;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-    @Bean
-    protected InMemoryUserDetailsManager userDetailsService(){
-        UserDetails userDetails = User
-                .withUsername("luka")
-                .password("{noop}password")
-                .roles("USER")
-                .build();
-        UserDetails userDetails2 = User
-                .withUsername("admin")
-                .password("{noop}password")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(userDetails,userDetails2);
+
+//    private final DataSource dataSource;
+//    @Autowired
+//    public WebSecurityConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//    @Autowired
+//    public void configAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder.jdbcAuthentication()
+//                .dataSource(dataSource)
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+//                .authoritiesByUsernameQuery("select username, role from users where username=?");
+//    }
+
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+//    @Bean
+//    protected InMemoryUserDetailsManager userDetailsService(){
+//        UserDetails userDetails = User
+//                .withUsername("luka")
+//                .password("{noop}password")
+//                .roles("USER")
+//                .build();
+//        UserDetails userDetails2 = User
+//                .withUsername("admin")
+//                .password("{noop}password")
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(userDetails,userDetails2);
+//    }
+    private final IUsersRepository usersRepository;
+
+    public WebSecurityConfig( IUsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
     }
 
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity http)throws Exception{
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsServiceImpl(usersRepository);
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http,AuthenticationManagerBuilder authenticationManagerBuilder)throws Exception{
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider());
         http
                 .authorizeHttpRequests(form->form
                         .requestMatchers(new AntPathRequestMatcher("/"))
                         .permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/edit/*")).hasRole("ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/edit/*")).hasAnyRole("ADMIN","EDITOR")
                         .requestMatchers(new AntPathRequestMatcher("/delete/*")).hasRole("ADMIN")
                         .anyRequest()
                         .authenticated())
@@ -52,7 +91,8 @@ public class WebSecurityConfig {
                         .permitAll())
                 .logout(form -> form
                 .logoutUrl("/logout")
-                .permitAll());
+                .permitAll())
+                .exceptionHandling(e->e.accessDeniedPage("/403"));
         return http.build();
     }
 }
